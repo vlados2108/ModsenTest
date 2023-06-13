573bd73c56bef26f092fd057f1ce396306158e8c
vlados2108 <verlov.vladislav@mail.ru>
2023-06-13 20:05:11 +0300

    added realization for crud operations

--- meetup-api/src/app.controller.ts
+++ meetup-api/src/app.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CreateDto } from './dto/create.dto';
+import { MeetupService } from './meetup/meetup.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,private readonly meetupService: MeetupService) {}
 
   @Get()
   getHello(): string {
@@ -12,28 +13,28 @@ export class AppController {
   }
 
   @Get()
-  getAllMeetups():any{
-
+  getAllMeetups(){
+    return this.meetupService.getMeetups()
   }
 
   @Get('get/:id')
   getMeetupById(@Param('id',ParseIntPipe)id:number){
-
+    return this.meetupService.getMeetup(id)
   }
 
   @UsePipes(new ValidationPipe())
   @Post('create')
   createMeetup(@Body() dto: CreateDto){ 
-    return dto
+    return this.meetupService.createMeetup(dto)
   }
 
   @Put('change/:id')
   changeMeetupInfo(@Param('id',ParseIntPipe) id:number, @Body() dto: CreateDto){
-
+    return this.meetupService.updateMeetup({id,data:dto})
   }
 
   @Delete('delete/:id')
   deleteMeetup(@Param('id',ParseIntPipe) id:number){
-
+    return this.meetupService.deleteMeetup(id)
   }
 }
