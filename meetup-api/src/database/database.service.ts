2f5e42d5413135bda1c8a337db1e85ebd263f299
vlados2108 <verlov.vladislav@mail.ru>
2023-06-13 20:03:24 +0300

    implemented prismaclient interface on databaseservice to work with prisma

--- meetup-api/src/database/database.service.ts
+++ meetup-api/src/database/database.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService extends PrismaClient implements OnModuleInit{
+ async onModuleInit(){
+    await this.$connect()
+ }
+
+ async onModuleDestroy(){
+    await this.$disconnect()
+ }
+
+ async enableShutdownHooks(app: INestApplication){
+    this.$on('beforeExit',async ()=>{
+        await app.close()
+    })
+ }
+}
