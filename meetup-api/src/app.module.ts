de2fcb46defed4edec0763ecea935111e89b5b09
vlados2108 <verlov.vladislav@mail.ru>
2023-06-13 20:02:06 +0300

    added 2 services to app module

--- meetup-api/src/app.module.ts
+++ meetup-api/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { UserService } from './user/user.service';
+import { MeetupService } from './meetup/meetup.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, MeetupService],
 })
 export class AppModule {}
