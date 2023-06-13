68cccf827b7a57d77437fb4b6678550e387bea13
vlados2108 <verlov.vladislav@mail.ru>
2023-06-13 11:01:12 +0300

    added validation to create-dto

--- meetup-api/src/dto/create.dto.ts
+++ meetup-api/src/dto/create.dto.ts
@@ -1,7 +1,19 @@
+import { IsDate, IsNumber, IsString } from "class-validator";
+import { isDate } from "util/types";
+
 export class CreateDto{
+    @IsString()
     name:string;
+
+    @IsString()
     description:string;
+
+    @IsString()
     tegs:string;
+
+    @IsDate()
     time: Date;
+
+    @IsString()
     place: string;
 }
