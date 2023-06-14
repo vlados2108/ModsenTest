dc8094da4208b9e2f8810b038544a81b0dbdcf2e
vlados2108 <verlov.vladislav@mail.ru>
2023-06-14 23:27:28 +0300

    added returning status codes for bad cases

--- meetup-api/src/meetup/meetup.service.ts
+++ meetup-api/src/meetup/meetup.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { Meetup } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 import { CreateDto } from 'src/dto/create.dto';
@@ -8,30 +8,53 @@ export class MeetupService {
     constructor(private readonly databaseService: DatabaseService){}
 
     getMeetups = async ():Promise<Meetup[]> => {
-        return this.databaseService.meetup.findMany()
+        try {
+            return this.databaseService.meetup.findMany()
+        }catch(e){
+            throw new HttpException("can't get meetups",500)
+        }
+        
     }
 
     getMeetup = async (id:number):Promise<Meetup> => {
-        return this.databaseService.meetup.findUnique({
-            where:{id:id}
-        })
+        try{
+            return this.databaseService.meetup.findUnique({
+                where:{id:id}
+            })
+        }catch(e){
+            throw new HttpException(`can't get meetup with id ${id}`,500)
+        }
     }
 
     createMeetup = async (data:CreateDto): Promise<Meetup> => {
-        return this.databaseService.meetup.create({data})
+        try{
+            return this.databaseService.meetup.create({data})
+        }catch(e){
+            throw new HttpException("can't create meetup",500)
+        }
     }
 
     updateMeetup = async (params:{id:number,data:CreateDto}):Promise<Meetup> => {
-        const {id,data} = params
-        return this.databaseService.meetup.update({
-            data:data,
-            where:{id:id}
-        })
+        try{
+            const {id,data} = params
+            return this.databaseService.meetup.update({
+                data:data,
+                where:{id:id}
+            })
+        }catch(e){
+            throw new HttpException("can't update meetup",500)
+        }
+   
     }
 
     deleteMeetup = async (id:number): Promise<Meetup> => {
-        return this.databaseService.meetup.delete({
-            where: {id:id}
-        })
+        try{
+            return this.databaseService.meetup.delete({
+                where: {id:id}
+            })
+        }catch(e){
+            throw new HttpException("can't delete meetup",500)
+        }
+       
     }
 }
