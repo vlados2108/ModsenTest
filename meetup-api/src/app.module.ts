4bb28574bdd906fab41568e71c496922055b4de5
vlados2108 <verlov.vladislav@mail.ru>
2023-06-16 00:46:31 +0300

    added some imports

--- meetup-api/src/app.module.ts
+++ meetup-api/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { UserService } from './user/user.service';
 import { MeetupService } from './meetup/meetup.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService, UserService, MeetupService],
+  providers: [AppService, UsersService, MeetupService],
 })
 export class AppModule {}
