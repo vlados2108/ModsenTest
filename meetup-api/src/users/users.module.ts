a2bee305283bdaddad40e1100489ed94438c616d
vlados2108 <verlov.vladislav@mail.ru>
2023-06-18 13:47:10 +0300

    added some dependencies

--- meetup-api/src/users/users.module.ts
+++ meetup-api/src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { DatabaseService } from 'src/database/database.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  providers: [UsersService],
-  exports:[UsersModule]
+  providers: [UsersService,DatabaseService],
+  exports:[UsersModule,UsersService],
+  imports:[]
 })
 export class UsersModule {}
