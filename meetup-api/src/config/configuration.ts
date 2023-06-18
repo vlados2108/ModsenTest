1e50e3270b0d7bcafd8e34157a4cd525b085f3c9
vlados2108 <verlov.vladislav@mail.ru>
2023-06-18 17:08:08 +0300

    added is public key constant

--- meetup-api/src/config/configuration.ts
+++ meetup-api/src/config/configuration.ts
@@ -4,4 +4,5 @@ export default () =>({
     port : parseInt(process.env.PORT,10)||3000,
     databaseUrl: env.DATABASE_URL,
     secretKey: process.env.SECRET_KEY,
+    isPublicKey: process.env.IS_PUBLIC_KEY,
 })
