925b750e1970b7327190e870f0060f4cb87dd1eb
vlados2108 <verlov.vladislav@mail.ru>
2023-06-18 13:43:28 +0300

    registered JwtModule

--- meetup-api/src/auth/auth.module.ts
+++ meetup-api/src/auth/auth.module.ts
@@ -3,9 +3,27 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseService } from 'src/database/database.service';
+import { UsersService } from 'src/users/users.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule,PassportModule],
-  providers: [AuthService,LocalStrategy]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory: async (configService:ConfigService) => ({
+        secret:configService.get<string>('secretKey'),
+        signOptions:{expiresIn:'60s'}
+      })
+    })
+  ],
+  providers: [AuthService,LocalStrategy],
+  exports:[AuthService]
 })
 export class AuthModule {}
+  
