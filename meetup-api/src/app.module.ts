5db2b3131e9b803f2b0715e6db63a675de4cd292
vlados2108 <verlov.vladislav@mail.ru>
2023-06-18 13:41:21 +0300

    added configModule for all modules

--- meetup-api/src/app.module.ts
+++ meetup-api/src/app.module.ts
@@ -6,9 +6,14 @@ import { MeetupService } from './meetup/meetup.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { UsersService } from './users/users.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, UsersModule],
+  imports: [DatabaseModule, AuthModule, UsersModule,ConfigModule.forRoot({
+    isGlobal:true,
+    load:[configuration],
+  })],
   controllers: [AppController],
   providers: [AppService, UsersService, MeetupService],
 })
