1dd3750e63c1ce256d50c4dd56a0e786f0fec84b
vlados2108 <verlov.vladislav@mail.ru>
2023-06-18 13:43:02 +0300

    port now receives valuse from config file

--- meetup-api/src/main.ts
+++ meetup-api/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const configService = app.get(ConfigService)
   const config = new DocumentBuilder()
     .setTitle('Meetup API')
     .setDescription('The meetup API description')
@@ -13,7 +14,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
- 
-  await app.listen(3000);
+  await app.listen(configService.get<number>('port'));
 }
 bootstrap();
