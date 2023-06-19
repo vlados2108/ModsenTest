372eccfba74d0bb5288fed0f39afa3eb1900968e
vlados2108 <verlov.vladislav@mail.ru>
2023-06-19 16:45:04 +0300

    beatyfied the code

--- meetup-api/src/main.ts
+++ meetup-api/src/main.ts
@@ -6,20 +6,17 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService)
+  const configService = app.get(ConfigService);
   const config = new DocumentBuilder()
     .setTitle('Meetup API')
     .setDescription('The meetup API description')
     .setVersion('1.0')
     .addTag('meetups')
     .build();
-  
-  app.use(cookieParser())
+
+  app.use(cookieParser());
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(configService.get<number>('port'));
-
-
 }
 bootstrap();
- 
