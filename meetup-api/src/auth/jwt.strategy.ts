823faa25ffa68ef21654969a41cdc35d2887b589
vlados2108 <verlov.vladislav@mail.ru>
2023-06-19 12:49:06 +0300

    changed the way jwt token sends from client

--- meetup-api/src/auth/jwt.strategy.ts
+++ meetup-api/src/auth/jwt.strategy.ts
@@ -2,16 +2,25 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly configService:ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        JwtStrategy.extractJWTFromCookie,
+      ]),
       ignoreExpiration: false,
       secretOrKey: configService.get<string>("secretKey"),
     });
+  }
+
+  private static extractJWTFromCookie(req: Request): string | null {
+    if (req.cookies && req.cookies.access_token) {
+      return req.cookies.access_token;
+    }
+    return null;
   }
 
   async validate(payload: any) {
