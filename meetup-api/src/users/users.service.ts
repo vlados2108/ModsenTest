38640040cdc7a4ce1775a61ff0d3a5a27875d030
vlados2108 <verlov.vladislav@mail.ru>
2023-06-20 13:25:58 +0300

    feat: added functions for geting user and updating him with new refreshtoken

--- meetup-api/src/users/users.service.ts
+++ meetup-api/src/users/users.service.ts
@@ -17,6 +17,23 @@ export class UsersService {
     return user;
   };
 
+  getUserById = async (userId: number): Promise<User> => {
+    const user = await this.databaseService.user.findUnique({
+      where: { id: userId },
+    });
+    if (!user) throw new NotFoundException('user not found');
+
+    return user;
+  }
+
+  setCurrentRefreshToken  = async (refreshToken:string,userId:number) => {
+    const currentHashedRefreshToken = await bcrypt.hash(refreshToken,10)
+    const res = await this.databaseService.user.update({
+      where:{id:userId},
+      data:{hashedRefreshToken:currentHashedRefreshToken}
+    })
+  }
+
   createUser = async (data: UserDto): Promise<User> => {
     const hash = bcrypt.hashSync(data.password, 10);
     data.password = hash;
@@ -24,4 +41,24 @@ export class UsersService {
     if (!res) throw new HttpException(`can't create user}`, 409);
     return res;
   };
+
+  async getUserIfRefreshTokenMatches(refreshToken: string, userId: number) {
+    const user = await this.getUserById(userId);
+ 
+    const isRefreshTokenMatching = await bcrypt.compare(
+      refreshToken,
+      user.hashedRefreshToken
+    );
+ 
+    if (isRefreshTokenMatching) {
+      return user;
+    }
+  }
+
+  async removeRefreshToken(userId: number) {
+    return this.databaseService.user.update({
+      where:{id:userId},
+      data:{hashedRefreshToken:null}
+    });
+  }
 }
