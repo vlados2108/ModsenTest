3a549719b42db1aa83d6b8faa10b7949379cfc1d
vlados2108 <verlov.vladislav@mail.ru>
2023-06-20 13:24:10 +0300

    feat: added conf variables for working with jwt

--- meetup-api/src/config/configuration.ts
+++ meetup-api/src/config/configuration.ts
@@ -4,5 +4,8 @@ export default () =>({
     port : parseInt(process.env.PORT,10)||3000,
     databaseUrl: env.DATABASE_URL,
     secretKey: process.env.SECRET_KEY,
+    accessExpiresIn : process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
+    refreshSecret: process.env.JWT_REFRESH_SECRET,
+    refreshExpiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME,
     isPublicKey: process.env.IS_PUBLIC_KEY,
 })
