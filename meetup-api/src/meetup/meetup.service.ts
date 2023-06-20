fe1e618807a9aafd808a9c48b18baa2712091d44
vlados2108 <verlov.vladislav@mail.ru>
2023-06-20 16:22:35 +0300

    feat: created getMeetups with options and getAllMetups without

--- meetup-api/src/meetup/meetup.service.ts
+++ meetup-api/src/meetup/meetup.service.ts
@@ -3,13 +3,60 @@ import { Meetup } from '@prisma/client';
 import { empty } from '@prisma/client/runtime';
 import { DatabaseService } from 'src/database/database.service';
 import { CreateMeetupDto } from 'src/meetup/dto/createMeetup.dto';
+import { FilterDto } from './dto/filter.dto';
 
 @Injectable()
 export class MeetupService {
   constructor(private readonly databaseService: DatabaseService) {}
 
-  getMeetups = async (): Promise<Meetup[]> => {
-    const res = await this.databaseService.meetup.findMany();
+  getMeetups = async (options: FilterDto): Promise<Meetup[]> => {
+    const { search, filterLow, filterHigh, sort, page ,perPage } = options;
+    let res = await this.databaseService.meetup.findMany({});
+    if (search)
+      res = res.filter(
+        (meetup) =>
+          meetup.name.includes(search) ||
+          meetup.description.includes(search) ||
+          meetup.place.includes(search) ||
+          meetup.tegs.includes(search) ||
+          meetup.time.toString().includes(search),
+      );
+
+    if (filterLow)
+      res = res.filter((meetup) => meetup.time > new Date(filterLow));
+
+    if (filterHigh)
+      res = res.filter((meetup) => meetup.time < new Date(filterHigh));
+
+    if (sort)
+      if (sort == 'asc')
+        res.sort((a, b) => {
+          if (a.time < b.time) return -1;
+          if (a.time > b.time) return 1;
+          return 0;
+        });
+      else if (sort == 'desc')
+      res.sort((a, b) => {
+        if (a.time < b.time) return 1;
+        if (a.time > b.time) return -1;
+        return 0;
+      });
+
+    if (page && perPage){
+      const startIndex = (page-1)*perPage
+      const endindex = startIndex + perPage
+
+      res = res.slice(startIndex,endindex)
+    }
+
+    if (!res) {
+      throw new NotFoundException(`Meetups not found`);
+    }
+    return res;
+  };
+
+  getAllMeetups = async (): Promise<Meetup[]> => {
+    const res = await this.databaseService.meetup.findMany({});
     if (!res) {
       throw new NotFoundException(`Meetups not found`);
     }
@@ -17,10 +64,10 @@ export class MeetupService {
   };
 
   getMeetupById = async (id: number): Promise<Meetup> => {
-    //throw new NotFoundException(`Meetup with ID ${id} not found`); 
+    //throw new NotFoundException(`Meetup with ID ${id} not found`);
     const res = await this.databaseService.meetup.findUnique({
       where: { id: id },
-    }); 
+    });
     if (!res) {
       throw new NotFoundException(`Meetup with ID ${id} not found`);
     }
