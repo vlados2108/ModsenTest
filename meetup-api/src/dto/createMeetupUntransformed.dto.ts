560a516a2dad08c16b9988c286e42e65df0201a7
vlados2108 <verlov.vladislav@mail.ru>
2023-06-22 14:51:45 +0300

    feat: added @ApiProperty decorator to show those properties in swagger

--- meetup-api/src/dto/createMeetupUntransformed.dto.ts
+++ meetup-api/src/dto/createMeetupUntransformed.dto.ts
@@ -1,18 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsDate, IsString } from "class-validator";
 
 export class CreateMeetupUntransformedDto{
     @IsString()
+    @ApiProperty()
     name:string;
 
     @IsString()
+    @ApiProperty()
     description:string;
 
     @IsString()
+    @ApiProperty()
     tegs:string;
 
     @IsString()
+    @ApiProperty()
     time: string;
 
     @IsString()
+    @ApiProperty()
     place: string;
 }
--- meetup-api/src/users/dto/user.dto.ts
+++ meetup-api/src/users/dto/user.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
 export class UserDto{
     @IsString()
+    @ApiProperty()
     name:string;
 
     @IsString()
+    @ApiProperty()
     password:string;
 }
