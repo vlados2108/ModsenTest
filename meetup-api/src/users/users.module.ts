6cc553682db98c264d97b7cdc9ffc2e860dd4035
vlados2108 <verlov.vladislav@mail.ru>
2023-06-23 16:04:38 +0300

    feat: added jwtService as a dependency

--- meetup-api/src/users/users.module.ts
+++ meetup-api/src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { DatabaseService } from 'src/database/database.service';
 import { DatabaseModule } from 'src/database/database.module';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [UsersService,DatabaseService],
+  providers: [UsersService,DatabaseService,JwtService],
   exports:[UsersModule,UsersService],
   imports:[]
 })
